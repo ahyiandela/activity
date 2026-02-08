08cfa58f0eb552a968820a2c34709b2d733bf3bf
ahyiandela <ahyiandela@gmail.com>
2026-02-08 10:43:41 +0800

    Add files via upload
    03_activity_1

---
+++ quijano_ahyian_mathapp/quijano_ahyian_mathapp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MathApp
+{
+    class Mathprogram
+    {
+        static void Main(string[] args)
+        {
+            double number = -9.75;
+            double baseNumber = 4;
+            double exponent = 2;
+            double decimalvalue = 6.3;
+
+            double absoluteValue = Math.Abs(number);
+            double squareroot = Math.Sqrt(absoluteValue);
+            double powerResult = Math.Pow(baseNumber, exponent);
+            double roundedVAlue = Math.Round(decimalvalue);
+            double cellingValue = Math.Ceiling(decimalvalue);
+            double floorValue = Math.Floor(decimalvalue);
+
+            Console.WriteLine("Math Class");
+            Console.WriteLine("Absolute Value: " + absoluteValue);
+            Console.WriteLine("Square root: " + squareroot);
+            Console.WriteLine("Power Result: " + powerResult);
+            Console.WriteLine("Rounded Value: " + roundedVAlue);
+            Console.WriteLine("Ceiling Value: " + cellingValue);
+            Console.WriteLine("Floor Value: " + floorValue);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ quijano_ahyian_mathapp/quijano_ahyian_mathapp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("quijano_ahyian_mathapp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("quijano_ahyian_mathapp")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b91707d6-4888-47e9-91da-d41d096d3298")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
